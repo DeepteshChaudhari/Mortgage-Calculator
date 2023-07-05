6c95595930a6c3f15bb53e7a018e91dfbcc44649
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-07-05 21:07:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll(`.inputs`);
+const outputs = document.querySelector(`.outputDiv`);
+const outputButton = document.querySelector(`.outputBtn`);
+
+outputButton.addEventListener(`click`, calculate);
+
+function getValue(p, r, n) {
+  return (p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+}
+
+function calculate() {
+  const principal = Number(inputs[0].value);
+  const interestRate = Number(inputs[1].value) / 100;
+  const loanTerm = Number(inputs[2].value) * 12;
+
+  const monthlyPayment = getValue(principal, interestRate / 12, loanTerm);
+  outputs.innerText = `Monthly payment of yor Mortgage is ${monthlyPayment.toFixed(
+    2
+  )}`;
+}
